9a9c5be402aa0466143e87d3e58ecb855c4e5329
momodev48 <95142395+momodev48@users.noreply.github.com>
2021-11-27 18:51:35 +0100

    Add files via upload

---
+++ src/scripts/app.js
@@ -0,0 +1,21 @@
+console.info('Hello world');
+/*solution__accordion*/
+
+var accordion = document.getElementsByClassName("solution__accordion");
+
+var i;
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
